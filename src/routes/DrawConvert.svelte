7e095c692b6183b872f4d999dc720b9b9daa81bc
Myrrh Khan <myrrhkhan@gmail.com>
2024-07-23 22:47:09 -0400

    starter code without excalidraw

---
+++ src/routes/DrawConvert.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import { ReactAdapter } from 'svelte-react-kit';
+	// import Whiteboard from '../lib/components/Whiteboard';
+</script>
+
+<!-- <ReactAdapter el={Whiteboard} /> -->
